72c963af359f8e4b0dfbcdc4ab329ff68518481b
Bekzat Yernat <gimtimes@gmail.com>
2022-11-13 22:22:11 +0600

    changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	$('#search-query').focus();
 });
 
+$('#search-btn').click(function() {
+	console.log('fd')
+	$('#results').addClass('results--active');
+});
+
 $('#advanced-search-button').click(function() {
 	$('.advanced-search-panel').slideToggle('fast');
 	$(this).toggleClass('active');
